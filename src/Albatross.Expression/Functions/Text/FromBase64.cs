6fc607341305d7229c8c40b49895edbb1f36ef2f
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2022-10-03 12:10:47 +0300

    Add ToBase64, FromBase64 and ToJsonFormat functions

---
+++ src/Albatross.Expression/Functions/Text/FromBase64.cs
@@ -0,0 +1,52 @@
+﻿using Albatross.Expression.Documentation;
+using Albatross.Expression.Documentation.Attributes;
+using Albatross.Expression.Tokens;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Albatross.Expression.Functions.Text
+{
+    [FunctionDoc(Group.Text, "{token}(@text)",
+        @"
+### Transforms any base64 string into an object.
+#### Inputs:
+- text: Any
+
+#### Outputs:
+- Object
+
+#### References:
+- [{token}](https://help.workiom.com/article/formula#{token})
+        ",
+        @"
+{token}(""MQ=="")
+        "
+    )]
+    [ParserOperation]
+    public class FromBase64 : PrefixOperationToken
+    {
+        public override string Name { get { return "FromBase64"; } }
+        public override int MinOperandCount { get { return 1; } }
+        public override int MaxOperandCount { get { return 1; } }
+        public override bool Symbolic { get { return false; } }
+
+        public override object EvalValue(Func<string, object> context)
+        {
+            object value = Operands.First().EvalValue(context);
+            return Base64ToObject(value?.ToString());
+        }
+
+        private object Base64ToObject(string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+            using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
+            {
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Position = 0;
+                return new BinaryFormatter().Deserialize(ms);
+            }
+        }
+    }
+}
---
+++ src/Albatross.Expression/Functions/Text/ToBase64.cs
@@ -0,0 +1,50 @@
+﻿using Albatross.Expression.Documentation;
+using Albatross.Expression.Documentation.Attributes;
+using Albatross.Expression.Tokens;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Albatross.Expression.Functions.Text
+{
+    [FunctionDoc(Group.Text, "{token}(@object)",
+        @"
+### Transforms any object into base64 string.
+#### Inputs:
+- object: Any
+
+#### Outputs:
+- String
+
+#### References:
+- [{token}](https://help.workiom.com/article/formula#{token})
+        ",
+        @"
+{token}(1)
+        "
+    )]
+    [ParserOperation]
+    public class ToBase64 : PrefixOperationToken
+    {
+        public override string Name { get { return "ToBase64"; } }
+        public override int MinOperandCount { get { return 1; } }
+        public override int MaxOperandCount { get { return 1; } }
+        public override bool Symbolic { get { return false; } }
+
+        public override object EvalValue(Func<string, object> context)
+        {
+            object value = Operands.First().EvalValue(context);
+            return ObjectToBase64(value);
+        }
+
+        private string ObjectToBase64(object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(ms, obj);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+    }
+}
