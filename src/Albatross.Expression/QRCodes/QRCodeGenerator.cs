125035b97755ffca879cb92bb6d61c130c8967f5
MorjanM <morjanmt@gmail.com>
2025-08-01 11:24:39 +0300

    Add QR code generation support
    - Integrated SkiaSharp and SkiaSharp.QrCode libraries.
    - Added `CreateQRCode` function to generate QR codes with configurable input, ECC level, and size.
    - Implemented `QRCodeGenerator` class for QR code creation.
    - Added unit tests to validate QR code generation functionality.

---
+++ src/Albatross.Expression/QRCodes/QRCodeGenerator.cs
@@ -0,0 +1,59 @@
+﻿using SkiaSharp;
+using SkiaSharp.QrCode;
+using System;
+using System.IO;
+
+namespace Albatross.Expression.QRCodes
+{
+    public class QRCodeGenerator
+    {
+        public byte[] Generate(string input, string ecc = "M", int size = 256)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
+            
+            var errorCorrectionLevel = GetErrorCorrectionLevel(ecc);
+            using (var generator = new SkiaSharp.QrCode.QRCodeGenerator())
+            {
+                var qr = generator.CreateQrCode(input, errorCorrectionLevel);
+                var info = new SKImageInfo(size, size);
+                using (var surface = SKSurface.Create(info))
+                {
+                    var canvas = surface.Canvas;
+                    canvas.Render(qr, info.Width, info.Height);
+                    using (var image = surface.Snapshot())
+                    {
+                        using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                        {
+                            byte[] result;
+                            using (var memoryStream = new MemoryStream())
+                            {
+                                data.SaveTo(memoryStream);
+                                result = memoryStream.ToArray();
+                            }
+                            
+                            return result;
+                        }
+                    }
+                }
+            }
+        }
+
+        private ECCLevel GetErrorCorrectionLevel(string ecc)
+        {
+            switch (ecc.ToUpper())
+            {
+                case "L":
+                    return ECCLevel.L;
+                case "M":
+                    return ECCLevel.M;
+                case "Q":
+                    return ECCLevel.Q;
+                case "H":
+                    return ECCLevel.H;
+                default:
+                    throw new ArgumentException($"Invalid ECC level: {ecc}. Supported: 'L', 'M', 'Q', 'H'.", nameof(ecc));
+            }
+        }
+    }
+}
